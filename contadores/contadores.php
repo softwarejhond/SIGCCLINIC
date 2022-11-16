b077ae527a336376ede3b7bd99509de57438e550
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 18:41:09 -0500

    nuevo

---
+++ contadores/contadores.php
@@ -0,0 +1,24 @@
+<?php
+require_once "conexion.php";
+$sql = mysqli_query($con,"SELECT * FROM patient");
+$numero = mysqli_num_rows($sql);
+    ?>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Cantidad de Usuarios</h5>
+        <h1 class="card-text"><?php echo $numero?></h1>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div
--- main.php
+++ main.php
@@ -47,6 +47,10 @@ $materiasCanceladas = mysqli_query($con, "SELECT * FROM investigacion WHERE padr
                         <?php include './dashboard/alertPatientDelete.php';?>                       
                         
                         <br>
+                        <?php //muy importante
+                         include "contadores/contadores.php";
+                        ?>
+                        <br>
                         <div class="container">
                             <ul class="nav nav-tabs" id="myTab" role="tablist">
                                 <li class="nav-item" role="presentation">
