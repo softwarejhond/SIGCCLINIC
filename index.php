bfe3eb63d03cb981cb960c77e93e44e5aa0590ed
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 03:20:09 -0500

    login

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ h6 {
     align-items: center;
     justify-content: center;
     height: 100vh;
-    background-image: url(images/fondo.png);
+    background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(images/fondo.png);
     box-shadow: 0 50px 70px -20px rgba(0, 0, 0, 0.85);
     background-size: cover;
 }
