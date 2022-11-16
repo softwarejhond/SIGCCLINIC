13a0cc5b60b52f5eb0cb4c37a4c3ae027476ba61
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 18:51:22 -0500

    solucion de error

--- contadores/contadores.php
+++ contadores/contadores.php
@@ -3,22 +3,33 @@ require_once "conexion.php";
 $sql = mysqli_query($con,"SELECT * FROM patient");
 $numero = mysqli_num_rows($sql);
     ?>
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Cantidad de Usuarios</h5>
-        <h1 class="card-text"><?php echo $numero?></h1>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Cantidad de Usuarios</h5>
+                    <h1 class="card-text"><?php echo $numero?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div
+</div>
