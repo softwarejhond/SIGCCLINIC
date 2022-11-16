fd9f0e49b6d31fee6e74f35832994376b4d3d326
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-15 21:34:29 -0500

    Actualiza credenciales

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 /*Datos de conexion a la base de datos*/
-$db_host = "15.235.65.10";
-$db_user = "cerifund";
-$db_pass = "QF6ewoe0[;C9T5";
-$db_name = "cerifund_ceri";
+$db_host = "157.90.91.29";
+$db_user = "agenciae";
+$db_pass = "q294g.#PwM9SYy";
+$db_name = "agenciae_sigc";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 mysqli_set_charset($con, 'utf8'); //Muy importante esta linea, guardara el contenido que contenga acentos de manera correcta configurando la bd con el UTF-8 spanis ci
