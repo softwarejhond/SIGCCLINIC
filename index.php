24870cf4e7d4b3d4c7f0fc017405b7445c700ec7
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 01:32:44 -0500

    login

--- index.php
+++ index.php
@@ -422,5 +422,7 @@ viewPassword.addEventListener('click', (e) => {
     }
 })
 </script>
-
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/21270650.js"></script>
+<!-- End of HubSpot Embed Code -->
 </html>
