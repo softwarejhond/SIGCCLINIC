a1f5b5d6b11c0fdbf659db94ab95ff56721c083e
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 22:00:55 -0500

    calendar

--- calendar.php
+++ calendar.php
@@ -2,8 +2,8 @@
 <style>
     @import url('https://fonts.googleapis.com/css?family=Nunito:400,600');
 :root {
-    --calendar-bg-color: #01A7F0;
-    --calendar-font-color: #FFF;
+    --calendar-bg-color: #fff;
+    --calendar-font-color: #000;
     --weekdays-border-bottom-color: #404040;
     --calendar-date-hover-color: #505050;
     --calendar-current-date-color: #1b1f21;
