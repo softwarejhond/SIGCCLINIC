fbaaba20df7b6289d3660b8e24f333cac442f263
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-16 19:52:58 -0500

    contador

--- contadores/contadores.php
+++ contadores/contadores.php
@@ -3,10 +3,12 @@ require_once "conexion.php";
 $sql = mysqli_query($con,"SELECT * FROM patient");
 $numero = mysqli_num_rows($sql);
 
-$sqlMan = mysqli_query($con,"SELECT * FROM patient WHERE genero='Masculino' AND genero='MASCULINO'");
+$sqlMan = mysqli_query($con,"SELECT * FROM patient WHERE genero='Masculino'");
+$sqlMAN = mysqli_query($con,"SELECT * FROM patient WHERE genero='MASCULINO'");
 $numeroMan = mysqli_num_rows($sqlMan);
-
-$sqlFemale = mysqli_query($con,"SELECT * FROM patient WHERE genero='Femenino' AND genero='FEMENINO");
+$numeroMAN = mysqli_num_rows($sqlMAN);
+$totalMan=$numeroMan+$numeroMAN;
+$sqlFemale = mysqli_query($con,"SELECT * FROM patient WHERE genero='Femenino'");
 $numeroFemale = mysqli_num_rows($sqlFemale);
     ?>
 <div class="container">
@@ -23,7 +25,7 @@ $numeroFemale = mysqli_num_rows($sqlFemale);
             <div class="card efecto alert-info">
                 <div class="card-body">
                     <h5 class="card-title">Hombres</h5>
-                     <h1 class="card-text"><i class="fa fa-male"></i> <?php echo $numeroMan?></h1>
+                     <h1 class="card-text"><i class="fa fa-male"></i> <?php echo $totalMan?></h1>
                 </div>
             </div>
         </div>
