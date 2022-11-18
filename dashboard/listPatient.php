7c6fb4eb5bfee5e794c7f8c799a7b74806e82e7d
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-18 17:23:58 -0500

    N

--- dashboard/listPatient.php
+++ dashboard/listPatient.php
@@ -1,9 +1,9 @@
 <div class="card text-center">
-     <div class="card-header" style="background-image:url(images/footer.png); color:#fff">
-         <i class="fas fa-user-injured"></i> LISTA DE PACIENTES <i class="fas fa-user-injured"></i>
-     </div>
-     <div class="card-body">
-     <?php
+    <div class="card-header" style="background-image:url(images/footer.png); color:#fff">
+        <i class="fas fa-user-injured"></i> LISTA DE PACIENTES <i class="fas fa-user-injured"></i>
+    </div>
+    <div class="card-body">
+        <?php
 include 'connect_test_db.php';
 $searchErr = '';
 $employee_details='';
@@ -25,41 +25,40 @@ if(isset($_POST['save']))
 }
 
 ?>
-	<div class="container">
-	<h3><u>PHP MySQL search database and display results</u></h3>
-	<br/><br/>
-	<form class="form-horizontal" action="#" method="post">
-	<div class="row">
-		<div class="form-group">
-		    <label class="control-label col-sm-4" for="email"><b>Search Employee Information:</b>:</label>
-		    <div class="col-sm-4">
-		      <input type="text" class="form-control" name="search" placeholder="search here">
-		    </div>
-		    <div class="col-sm-2">
-		      <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
-		    </div>
-		</div>
-		<div class="form-group">
-			<span class="error" style="color:red;">* <?php echo $searchErr;?></span>
-		</div>
-		
-	</div>
-    </form>
-	<br/><br/>
-	<h3><u>Search Result</u></h3><br/>
-	<div class="table-responsive">          
-	  <table class="table">
-	    <thead>
-	      <tr>
-	        <th>#</th>
-	        <th>Employee Name</th>
-	        <th>Phone No</th>
-	        <th>Age</th>
-	        <th>Department</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	    		<?php
+        <h3><u>PHP MySQL search database and display results</u></h3>
+        <br /><br />
+        <form class="form-horizontal" action="#" method="post">
+            <div class="row">
+                <div class="form-group">
+                    <label class="control-label col-sm-4" for="email"><b>Search Employee Information:</b>:</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" name="search" placeholder="search here">
+                    </div>
+                    <div class="col-sm-2">
+                        <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
+                </div>
+
+            </div>
+        </form>
+        <br /><br />
+        <h3><u>Search Result</u></h3><br />
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Employee Name</th>
+                        <th>Phone No</th>
+                        <th>Age</th>
+                        <th>Department</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
 		    	 if(!$employee_details)
 		    	 {
 		    		echo '<tr>No data found</tr>';
@@ -68,42 +67,42 @@ if(isset($_POST['save']))
 		    	 	foreach($employee_details as $key=>$value)
 		    	 	{
 		    	 		?>
-		    	 	<tr>
-		    	 		<td><?php echo $key+1;?></td>
-		    	 		<td><?php echo $value['name'];?></td>
-		    	 		<td><?php echo $value['phone_no'];?></td>
-		    	 		<td><?php echo $value['age'];?></td>
-		    	 		<td><?php echo $value['department'];?></td>
-		    	 	</tr>
-		    	 		
-		    	 		<?php
+                    <tr>
+                        <td><?php echo $key+1;?></td>
+                        <td><?php echo $value['name'];?></td>
+                        <td><?php echo $value['phone_no'];?></td>
+                        <td><?php echo $value['age'];?></td>
+                        <td><?php echo $value['department'];?></td>
+                    </tr>
+
+                    <?php
 		    	 	}
 		    	 	
 		    	 }
 		    	?>
-	    	
-	     </tbody>
-	  </table>
-	</div>
-</div>
-<script src="jquery-3.2.1.min.js"></script>
-         <table id="myTable" class=" table table-hover table-bordered table-lg table-responsive">
-             <thead class="thead-dark">
-                 <tr>
-                     <th>#</th>
-                     <th>Documento</th>
-                     <th class="w-25">Nombre</th>
-                     <th class="w-25">Apellidos</th>
-                     <th class="w-10">Celular</th>
-                     <th class="w-50">Doctor</th>
-                     <th class="w-50"> </th>
-                     <th class="w-50"> </th>
-                     <th class="w-50"> </th>
-                     <th class="w-50"> </th>
-                 </tr>
-             </thead>
-             <tbody>
-                 <?php
+
+                </tbody>
+            </table>
+
+        </div>
+        <script src="jquery-3.2.1.min.js"></script>
+        <table id="myTable" class=" table table-hover table-bordered table-lg table-responsive">
+            <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Documento</th>
+                    <th class="w-25">Nombre</th>
+                    <th class="w-25">Apellidos</th>
+                    <th class="w-10">Celular</th>
+                    <th class="w-50">Doctor</th>
+                    <th class="w-50"> </th>
+                    <th class="w-50"> </th>
+                    <th class="w-50"> </th>
+                    <th class="w-50"> </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
 
 $buscar = $_POST["buscador"];
 $usaurio = htmlspecialchars($_SESSION["numeroIdentificacion"]);
@@ -138,20 +137,20 @@ if (mysqli_num_rows($sql) == 0) {
     }
 }
 ?>
-             </tbody>
-         </table>
-     </div>
-     <div class="card-footer " style="background-image:url(images/footer.png); color:#fff">
-         <i class="fas fa-clock"></i>
-         <?php
+            </tbody>
+        </table>
+    </div>
+    <div class="card-footer " style="background-image:url(images/footer.png); color:#fff">
+        <i class="fas fa-clock"></i>
+        <?php
                                         $DateAndTime = date('m-d-Y h:i:s a', time());
                                         echo "Actualizado $DateAndTime.";
                                     ?>
-     </div>
+    </div>
 
- </div>
- <script>
-        $(document).ready(function() {
-            $(".toastPatient").toast('show');
-        });
+</div>
+<script>
+$(document).ready(function() {
+    $(".toastPatient").toast('show');
+});
 </script>
