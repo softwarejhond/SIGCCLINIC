b2af2d39d3a07793f62d100b08edcdabc775e449
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-18 17:49:17 -0500

    n

--- dashboard/listPatient.php
+++ dashboard/listPatient.php
@@ -57,7 +57,7 @@
                                                     <td><?= $items['numeroIdentificacion']; ?></td>
                                                     <td><?= $items['nombre']; ?></td>
                                                     <td><?= $items['apellidos']; ?></td>
-                                                    <td><a href="historiaClinica.php?nik='. $items['numeroIdentificacion'] . '" title="Realizar historia clínica" class="btn btn-outline-success btn-sm"><span class="fa fa-laptop-medical" aria-hidden="true"></span></a></td>
+                                                    <td><a href="historiaClinica.php?nik='<?= $items['numeroIdentificacion']?>'" title="Realizar historia clínica" class="btn btn-outline-success btn-sm"><span class="fa fa-laptop-medical" aria-hidden="true"></span></a></td>
                             <td><a href="evolucionesClinicas.php?nik=' . $items['numeroIdentificacion'] . '" title="Realizar valoración clínica" class="btn btn-outline-info btn-sm"><span class="fa fa-feather-alt" aria-hidden="true"></span></a></td>
                             <td><a href="upd_paciente.php?nik=' . $items['numeroIdentificacion'] . '" title="Editar paciente" class="btn btn-outline-warning btn-sm"><span class="fa fa-edit" aria-hidden="true"></span></a></td>
                             <td><a href="main.php?aksi=delete&nik=' . $items['numeroIdentificacion'] . '" title="Eliminar paciente" onclick=" return confirm(\'Esta seguro de borrar al paciente ' . $row['nombre'] . " " . $row['apellidos'] . '?\')" class="btn btn-outline-danger btn-sm"><span class="fa fa-trash" aria-hidden="true"></span></a></td>
