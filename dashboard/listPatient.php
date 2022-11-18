df533a7e2ad770f9de0e4dbcc10d93696486e511
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-18 18:05:55 -0500

    N

--- dashboard/listPatient.php
+++ dashboard/listPatient.php
@@ -29,7 +29,7 @@
                                     if(isset($_GET['search']))
                                     {
                                         $filtervalues = $_GET['search'];
-                                        $query = "SELECT * FROM patient WHERE numeroIdentificacion ='$filtervalues' ";
+                                        $query = "SELECT * FROM patient WHERE numeroIdentificacion LIKE '%$filtervalues%' ";
                                         $query_run = mysqli_query($con, $query);
 
                                         if(mysqli_num_rows($query_run) > 0)
