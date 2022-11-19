797e1e34a4bb4f00808a7cf38bf1f85372838cb8
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-19 00:16:25 -0500

    n

--- footer.php
+++ footer.php
@@ -30,3 +30,4 @@
 }
 </style>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/427e984aea.js" crossorigin="anonymous"></script>
