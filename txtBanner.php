5df836285dddb59c943cbe364835755a3f4f2b0f
Jhon Darwin Acevedo Munera <softwarejhond@gmail.com>
2022-11-19 00:42:43 -0500

    n

--- txtBanner.php
+++ txtBanner.php
@@ -18,7 +18,6 @@ session_start();
   include "conexion.php";
   ?>
 <div class="card bg-dark text-white">
-    <img src="images/encabezado.png" class="card-img w-100" alt="enccabezado" height="180px">
     <div class="container">
         <div class="card-img-overlay ">
             <img src="vista.php?id='<?php echo'9'?>'" alt='Perfil' class="rounded mr-1"
@@ -27,9 +26,9 @@ session_start();
                                $usaurio= htmlspecialchars($_SESSION["username"]);
                                $query = mysqli_query($con,"SELECT * FROM users WHERE username like '%$usaurio%'");
                                while ($userLog = mysqli_fetch_array($query)) {
-                                echo '<h5  class="card-text px-2 mt-1" style="color:#000;text-transform: uppercase;">'.$userLog['nombre'].'</h5>';  
-                                echo '<h5  class="card-text px-2" style="color:#000;text-transform: uppercase;">'.$userLog['profesion'].'</h5>';
-                                echo '<h5  class="card-text px-2" style="color:#000;text-transform: uppercase;">'.$userLog['dependencia'].'</h5>';
+                                echo '<h6  class="card-text px-2 mt-1" style="color:#000;text-transform: uppercase;">'.$userLog['nombre'].'</h6>';  
+                                echo '<h6  class="card-text px-2" style="color:#000;text-transform: uppercase;">'.$userLog['profesion'].'</h6>';
+                                echo '<h6  class="card-text px-2" style="color:#000;text-transform: uppercase;">'.$userLog['dependencia'].'</h6>';
                                     
                               }
                                 ?>
